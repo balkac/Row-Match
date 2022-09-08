0db1c081770c4ba64f0836eb7c5c2e4a34a919b5
balkac <furkanbalkac@gmail.com>
2022-09-08 09:32:44 +0300

    Camera Adjustments

--- Assets/Scripts/Camera/CameraSizer.cs
+++ Assets/Scripts/Camera/CameraSizer.cs
@@ -3,7 +3,10 @@
 public class CameraSizer : MonoBehaviour
 {
     private Grid _grid;
-
+    public float AspectRatio = 0.625f;
+    public float Padding = 2;
+    public GameObject UI;
+    public float UIScale = 0.2f;
     private void Awake()
     {
         _grid = FindObjectOfType<Grid>();
@@ -23,13 +26,28 @@ public class CameraSizer : MonoBehaviour
 
     private void OnGridInitialized(LevelData obj)
     {
-        RepositionCamera(_grid.Width, _grid.Height);
+        RepositionCamera(_grid.Width-1, _grid.Height-1);
     }
 
     private void RepositionCamera(float x, float y)
     {
-        Vector2 tempPosition = new Vector2(Mathf.Round((x-0.01f) / 2f), Mathf.Round(y / 2));
+        Vector2 tempPosition = new Vector2(x/2, y/2);
         transform.position = new Vector3(tempPosition.x, tempPosition.y, transform.position.z);
+        if (_grid.Width >= _grid.Height)
+        {
+            Camera.main.orthographicSize = (_grid.Width / 2 + Padding) / AspectRatio;
+        }
+        else
+        {
+            Camera.main.orthographicSize = _grid.Height / 2 + Padding;
+        }
+
+        if (Camera.main.orthographicSize>6)
+        {
+            UI.transform.localScale += Vector3.one * ((Camera.main.orthographicSize - 6) * UIScale);
+            transform.position += Vector3.up * ((Camera.main.orthographicSize - 6) * UIScale);
+        }
+        
     }
     
 }
